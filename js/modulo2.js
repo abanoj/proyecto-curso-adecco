2d617c46426c450f88569592ba43905217dd8af7
Jesús Abano <jaabanof@gmail.com>
2023-05-04 14:15:45 +0200

    Añadido modulo2.js y otros cambios

--- js/modulo2.js
+++ js/modulo2.js
@@ -1,5 +1,6 @@
 class Usuario {
-    constructor(name, lastName, username, password, tipo, activo){
+    constructor(dni, name, lastName, username, password, tipo, activo){
+        this.dni = dni;
         this.name = name;
         this.lastName = lastName;
         this.username = username;
@@ -19,31 +20,69 @@ class Nota{
 }
 
 class Asignatura{
-    constructor(nombre, ...profesores){
-        this.nombre = nombre;
+    constructor(name, ...profesores){
+        this.name = name;
         this.idProfesores = profesores;
     }
 }
 
 class Cursos{
-    constructor(nombre, ...asignaturas){
-        this.nombre = nombre;
+    constructor(name, ...asignaturas){
+        this.name = name;
         this.asignaturas = asignaturas;
     }
 }
-// Creacion de usuarios
-let juan = new Usuario("Juan", "Perez", "juanperez", "123abc", "alumno", true);
-let maria = new Usuario("Maria", "Ramos", "mariar12", "a1b2c3", "alumno", false);
-let jose = new Usuario("José", "Estevez", "estevezjose", "prof20$5", "profesor", true);
-let pedro = new Usuario("Pedro", "Estevez", "pedroes", "pGsg20$5", "profesor", true);
-let rosa = new Usuario("Rosa", "Perez", "rosapez", "prda20$5", "profesor", true);
+// Creacion de Usuarios
+let juan = new Usuario("00523821F", "Juan", "Perez", "juanperez", "123abc", "alumno", true);
+let maria = new Usuario("0012381S", "Maria", "Ramos", "mariar12", "a1b2c3", "alumno", false);
+let ramon = new Usuario("0045281D", "Ramón", "Sanchez", "RanSanz121", "sg2c3", "alumno", true);
+let jose = new Usuario("Y9307718J", "José", "Estevez", "estevezjose", "prof20$5", "profesor", true);
+let pedro = new Usuario("Y8180092G", "Pedro", "Estevez", "pedroes", "pGsg20$5", "profesor", true);
+let rosa = new Usuario("Y6168872G", "Rosa", "Perez", "rosapez", "prda20$5", "profesor", true);
+const arrayUsuarios = [juan, maria, ramon, jose, pedro, rosa];
 
 // Creacion de Asignatura
-let matematica = new Asignatura("Matemáticas", jose.name, pedro.name);
-let fisica = new Asignatura("Matemáticas", pedro.name, jose.name);
-let quimica = new Asignatura("Matemáticas", rosa.name);
+let matematicas = new Asignatura("Matemáticas", jose.name, pedro.name);
+let fisica = new Asignatura("Física", pedro.name, jose.name);
+let quimica = new Asignatura("Química", rosa.name);
+const arrayAsignaturas = [matematicas, fisica, quimica];
+
 // Creacion de Cursos
-let curso2023 = new Cursos("Curso 2023", matematica.nombre, fisica.nombre, quimica.nombre);
-console.log(curso2023);
+let curso2021 = new Cursos("Curso 2021", matematicas.name, fisica.name, quimica.name);
+let curso2022 = new Cursos("Curso 2022", matematicas.name, fisica.name, quimica.name);
+let curso2023 = new Cursos("Curso 2023", matematicas.name, fisica.name, quimica.name);
+const arrayCursos = [curso2021, curso2022, curso2023];
 
+// Creacion de Notas
+let notaJuan1 = new Nota(juan.name, matematicas.name, curso2023.name, 8);
+let notaJuan2 = new Nota(juan.name, fisica.name, curso2023.name, 10);
+let notaRamon1 = new Nota(ramon.name, quimica.name, curso2023.name, 6);
+let notaMaria1 = new Nota(maria.name, matematicas.name, curso2021.name, 9);
+let notaMaria2 = new Nota(maria.name, fisica.name, curso2021.name, 10);
+const arrayNotas = [notaJuan1, notaJuan2, notaRamon1, notaMaria1, notaMaria2];
 
+// for (const el of arrayNotas) {
+//     console.log(`Alumno: ${el.idAlumno}`);
+//     console.log(`Asignatura: ${el.idAsignatura}`);
+//     console.log(`Curso: ${el.curso}`);
+//     console.log(`Nota: ${el.nota}`);
+//     console.log("==================");
+// }
+
+const jsonUsuarios = JSON.stringify(arrayUsuarios);
+const jsonAsignaturas = JSON.stringify(arrayAsignaturas);
+const jsonCursos = JSON.stringify(arrayCursos);
+const jsonNotas = JSON.stringify(arrayNotas);
+
+//Llamado a API
+fetch("https://swapi.dev/api/people/")
+.then(res => (res.ok ? res.json(): Promise.reject(res)))
+.then(data => {
+    data.results.forEach(el => {
+        console.log(`Nombre: ${el.name}`);
+        console.log(`Altura: ${el.height}`);
+        console.log(`Color ojos: ${el.eye_color}`);
+        console.log("=====================");
+    });
+})
+.catch(reject => console.log(reject));
